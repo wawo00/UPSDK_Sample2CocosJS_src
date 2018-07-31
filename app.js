c4f3b985abeb1b75086bae9445e54684bf7f3d86
wanlei <roy.wan@upltv.com>
2018-07-31 15:59:04 +0800

    3003版本的src文件

---
+++ app.js
@@ -0,0 +1,441 @@
+var HelloWorldLayer = cc.Layer.extend({
+    sprite: null,
+    ctor: function() {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        /////////////////////////////
+        // 2. add a menu item with "X" image, which is clicked to quit the program
+        //    you may modify it.
+        // ask the window size
+        var size = cc.winSize;
+        size.wi cc.log("===> js size wd: %d", size.width);
+        cc.log("===> js size ht: %d", size.height);
+        /////////////////////////////
+        // 3. add your codes below...
+        // add a label shows "Hello Sam"
+        // create and initialize a label
+        var helloLabel = new cc.LabelTTF("Hello Js, By Sam", "Arial", 26);
+
+        var offtop = 16;
+        // position the label on the center of the screen
+        helloLabel.x = size.width / 2;
+        helloLabel.y = offtop;
+        // add the label as a child to this layer
+        this.addChild(helloLabel, 1);
+        // add "HelloWorld" splash screen"
+        this.sprite = new cc.Sprite(res.HelloWorld_png);
+
+        this.sprite.attr({
+            x: size.width / 2,
+            y: 32,
+            anchorX: 0.5,
+            anchorY: 0.0
+        });
+        this.addChild(this.sprite, 0);
+
+        var left = 0;
+        var top = 620 - 20;
+
+        var x = left;
+        var y = top;
+
+        var distHt = 90;
+        var disWd = 185;
+        // add button for test
+        var initSdkButton = this.createButton(x, y, "initSdk");
+        initSdkButton.addTouchEventListener(function(sender, type) {
+            cc.log("===> js initSdkButton action: %d", type);
+            if (type == 2) {
+                upltv.intSdk(0);
+            }
+        },
+        this);
+
+        y -= distHt;
+        var initSdkButton2 = this.createButton(x, y, "initSdkCall");
+        initSdkButton2.addTouchEventListener(function(sender, type) {
+            cc.log("===> js initSdkButton2 action: %d", type);
+            if (type == 2) {
+                upltv.intSdk(0,
+                function(r) {
+                    cc.log("===> js intSdk result:, %s", r);
+                });
+            }
+        },
+        this);
+
+        y -= distHt;
+        var initAbButton = this.createButton(x, y, "initABTest");
+        initAbButton.addTouchEventListener(function(sender, type) {
+            cc.log("===> js initAbButton action: %d", type);
+            if (type == 2) {
+                upltv.initAbtConfigJson("u89731", true, 0, "Facebook", "M", -1, ["This is the first element.", "The second one.", "The last one."]);
+            }
+        },
+        this);
+
+        y -= distHt;
+        var getAbButton = this.createButton(x, y, "getABConfig");
+        getAbButton.addTouchEventListener(function(sender, type) {
+            cc.log("===> js getABConfig action: %d", type);
+            if (type == 2) {
+                var r = upltv.getAbtConfig("pass");
+                cc.log("===> js getAbtConfig rr 3333: %s", r);
+            }
+        },
+        this);
+
+        // 第二行
+        x = left + disWd;
+        y = top;
+
+        var showRdUIButton = this.createButton(x, y, "rdDebugUI");
+        showRdUIButton.addTouchEventListener(function(sender, type) {
+            cc.log("===> js showRewardDebugUI action: %d", type);
+            if (type == 2) {
+                upltv.showRewardDebugUI();
+            }
+        },
+        this);
+
+        y -= distHt;
+        var rdLoadCallButton = this.createButton(x, y, "rdLoadCall");
+        rdLoadCallButton.addTouchEventListener(function(sender, type) {
+            cc.log("===> js setRewardVideoLoadCallback action: %d", type);
+            if (type == 2) {
+                upltv.setRewardVideoLoadCallback(function(cpid, msg) {
+                    cc.log("===> js RewardVideo LoadCallback Success at: %s", cpid);
+                },
+                function(cpid, msg) {
+                    cc.log("===> js RewardVideo LoadCallback Fail at: %s", cpid);
+                });
+            }
+        },
+        this);
+
+        y -= distHt;
+        var rdShowCallButton = this.createButton(x, y, "rdShowCall");
+        rdShowCallButton.addTouchEventListener(function(sender, type) {
+            cc.log("===> js setRewardVideoShowCallback action: %d", type);
+            if (type == 2) {
+                upltv.setRewardVideoShowCallback(function(type, cpid) {
+                    var event = "unkown";
+                    if (type == upltv.AdEventType.VIDEO_EVENT_DID_SHOW) {
+                        event = "Did_Show";
+                    } else if (type == upltv.AdEventType.VIDEO_EVENT_DID_CLICK) {
+                        event = "Did_Click";
+                    } else if (type == upltv.AdEventType.VIDEO_EVENT_DID_CLOSE) {
+                        event = "Did_Close";
+                    } else if (type == upltv.AdEventType.VIDEO_EVENT_DID_GIVEN_REWARD) {
+                        event = "Did_Given_Reward";
+                    } else if (type == upltv.AdEventType.VIDEO_EVENT_DID_ABANDON_REWARD) {
+                        event = "Did_Abandon_Reward";
+                    }
+                    cc.log("===> js RewardVideo Show Callback, event: %s, at: %s", event, cpid);
+                });
+            }
+        },
+        this);
+
+        y -= distHt;
+        var readyRdUIButton = this.createButton(x, y, "rdReady");
+        readyRdUIButton.addTouchEventListener(function(sender, type) {
+            cc.log("===> js isRewardReady action: %d", type);
+            if (type == 2) {
+                var r = upltv.isRewardReady();
+                cc.log("===> js isRewardReady r: %s", r.toString());
+            }
+        },
+        this);
+
+        y -= distHt;
+        var showRdUIButton = this.createButton(x, y, "rdShow");
+        showRdUIButton.addTouchEventListener(function(sender, type) {
+            cc.log("===> js showRewardVideo action: %d", type);
+            if (type == 2) {
+                var r = upltv.isRewardReady();
+                cc.log("===> js isRewardReady r: %s", r);
+                if (r == true) {
+                    cc.log("===> js showRewardVideo call");
+                    upltv.showRewardVideo();
+                }
+            }
+        },
+        this);
+
+        // 第三列插屏广告
+        x = left + disWd * 2;
+        y = top;
+        //var cpPlaceId = "inter_aaa";
+        var cpPlaceId = "inter_aaa";
+        //var cpPlaceId = "Interstitial_GameStart";
+        var ilShowUIButton = this.createButton(x, y, "ilShowUI");
+        ilShowUIButton.addTouchEventListener(function(sender, type) {
+            cc.log("===> js showInterstitialDebugUI action: %d", type);
+            if (type == 2) {
+                upltv.showInterstitialDebugUI();
+            }
+        },
+        this);
+
+        y -= distHt;
+        var ilLoadCallUIButton = this.createButton(x, y, "ilLoadCall");
+        ilLoadCallUIButton.addTouchEventListener(function(sender, type) {
+            cc.log("===> js setInterstitialLoadCallback action: %d", type);
+            if (type == 2) {
+                upltv.setInterstitialLoadCallback(cpPlaceId,
+                function(cpid, msg) {
+                    cc.log("===> js il load callback success: %s at placementid:%s", msg, cpid);
+                },
+                function(cpid, msg) {
+                    cc.log("===> js il load callback fail: %s at placementid:%s", msg, cpid);
+                });
+            }
+        },
+        this);
+
+        y -= distHt;
+        var ilShowCallUIButton = this.createButton(x, y, "ilShowCall");
+        ilShowCallUIButton.addTouchEventListener(function(sender, type) {
+            cc.log("===> js setInterstitialShowCallback action: %d", type);
+            if (type == 2) {
+                upltv.setInterstitialShowCallback(cpPlaceId,
+                function(type, cpid) {
+                    var event = "unkown";
+                    if (type == upltv.AdEventType.INTERSTITIAL_EVENT_DID_SHOW) {
+                        event = "Did_Show";
+                    } else if (type == upltv.AdEventType.INTERSTITIAL_EVENT_DID_CLICK) {
+                        event = "Did_Click";
+                    } else if (type == upltv.AdEventType.INTERSTITIAL_EVENT_DID_CLOSE) {
+                        event = "Did_Close";
+                    }
+                    cc.log("===> js il ad event: %s, at placementid: %s", event, cpid);
+                });
+            }
+        },
+        this);
+
+        y -= distHt;
+        var ilReadyAsynUIButton = this.createButton(x, y, "ilReadyAsyn");
+        ilReadyAsynUIButton.addTouchEventListener(function(sender, type) {
+            //cc.log("===> js isInterstitialReadyAsyn action: %d", type);
+            if (type == 2) {
+                upltv.isInterstitialReadyAsyn(cpPlaceId,
+                function(r) {
+                    cc.log("===> js il ad isreadyasyn: %s at placementid:%s", r, cpPlaceId);
+                });
+            }
+        },
+        this);
+
+        y -= distHt;
+        var ilShowUIButton = this.createButton(x, y, "ilShow");
+        ilShowUIButton.addTouchEventListener(function(sender, type) {
+            //cc.log("===> js isInterstitialReady action: %d", type);
+            if (type == 2) {
+                var r = upltv.isInterstitialReady(cpPlaceId);
+                cc.log("===> js il ad isready: %s at placementid:%s", r, cpPlaceId);
+                if (r == true) {
+                    upltv.showInterstitialAd(cpPlaceId);
+                }
+
+            }
+        },
+        this);
+
+        //第四列
+        x = left + disWd * 3;
+        y = top;
+
+        var bottomCpId = "banner_bbb";
+        var topCpId = "banner_aaa";
+
+        var bnCallButton = this.createButton(x, y, "SetBNCall");
+        bnCallButton.addTouchEventListener(function(sender, type) {
+            //cc.log("===> js showBannerAdAtTop action: %d", type);
+            if (type == 2) {
+                var bannerCall = function(type, cpadid) {
+                    var event = "unkown";
+                    if (type == upltv.AdEventType.BANNER_EVENT_DID_SHOW) {
+                        event = "Did_Show";
+                    } else if (type == upltv.AdEventType.BANNER_EVENT_DID_CLICK) {
+                        event = "Did_Click";
+                    } else if (type == upltv.AdEventType.BANNER_EVENT_DID_REMOVED) {
+                        event = "Did_Removed";
+                    }
+                    cc.log("=====> banner event: %s, at : %s", event, cpadid);
+                };
+
+                upltv.setBannerShowCallback(topCpId, bannerCall);
+
+                upltv.setBannerShowCallback(bottomCpId, bannerCall);
+            }
+        },
+        this);
+
+        y -= distHt;
+        var bnTopButton = this.createButton(x, y, "TopBNShow");
+        bnTopButton.addTouchEventListener(function(sender, type) {
+            //cc.log("===> js showBannerAdAtTop action: %d", type);
+            if (type == 2) {
+                upltv.setTopBannerPading(50);
+                upltv.showBannerAdAtTop(topCpId);
+            }
+        },
+        this);
+
+        y -= distHt;
+        var bnBottomButton = this.createButton(x, y, "BottomBNShow");
+        bnBottomButton.addTouchEventListener(function(sender, type) {
+            //cc.log("===> js showBannerAdAtTop action: %d", type);
+            if (type == 2) {
+                upltv.showBannerAdAtBottom(bottomCpId);
+            }
+        },
+        this);
+
+        y -= distHt;
+        var bnHideButton = this.createButton(x, y, "hideAllBN");
+        bnHideButton.addTouchEventListener(function(sender, type) {
+            //cc.log("===> js showBannerAdAtTop action: %d", type);
+            if (type == 2) {
+                upltv.hideBannerAdAtTop();
+                upltv.hideBannerAdAtBottom();
+            }
+        },
+        this);
+
+        y -= distHt;
+        var bnRemoveButton = this.createButton(x, y, "removeAllBn");
+        bnRemoveButton.addTouchEventListener(function(sender, type) {
+            //cc.log("===> js showBannerAdAtTop action: %d", type);
+            if (type == 2) {
+                upltv.removeBannerAdAt(topCpId);
+                upltv.removeBannerAdAt(bottomCpId);
+            }
+        },
+        this);
+
+        // 第五行
+        x = left + disWd * 4;
+        y = top;
+        //onBackPressed setManifestPackageName
+        var setPkgButton = this.createButton(x, y, "setPkg");
+        setPkgButton.addTouchEventListener(function(sender, type) {
+            if (type == 2) {
+                upltv.setManifestPackageName("com.js.test");
+            }
+        },
+        this);
+
+        y -= distHt;
+        var onBackButton = this.createButton(x, y, "onBackPressed");
+        onBackButton.addTouchEventListener(function(sender, type) {
+            if (type == 2) {
+                upltv.setBackPressedCallback(function(type, msg) {
+                    var event = "unkown";
+                    if (type == upltv.AdEventType.EXITAD_EVENT_DID_SHOW) {
+                        event = "Did_Show";
+                    } else if (type == upltv.AdEventType.EXITAD_EVENT_DID_CLICK) {
+                        event = "Did_Click";
+                    } else if (type == upltv.AdEventType.EXITAD_EVENT_DID_CLICKMORE) {
+                        event = "Did_Click_More";
+                    } else if (type == upltv.AdEventType.EXITAD_EVENT_DID_CLICKMORE) {
+                        event = "Did_Click_More";
+                    } else if (type == upltv.AdEventType.EXITAD_EVENT_DID_CANCEL) {
+                        event = "Did_Cancel";
+                    }
+                    cc.log("=====> exit ad event: %s, at : %s", event, cpadid);
+                });
+
+                upltv.onBackPressed();
+            }
+        },
+        this);
+
+        y -= distHt;
+        var loadManualButton = this.createButton(x, y, "loadByManual");
+        loadManualButton.addTouchEventListener(function(sender, type) {
+            //cc.log("===> js showBannerAdAtTop action: %d", type);
+            if (type == 2) {
+                upltv.loadAdsByManual();
+            }
+        },
+        this);
+
+        y -= distHt;
+        var exitAppButton = this.createButton(x, y, "exitApp");
+        exitAppButton.addTouchEventListener(function(sender, type) {
+            //cc.log("===> js showBannerAdAtTop action: %d", type);
+            if (type == 2) {
+                upltv.exitApp();
+            }
+        },
+        this);
+
+        y -= distHt;
+        var exitAppButton = this.createButton(x, y, "GDPR");
+
+        exitAppButton.addTouchEventListener(function(sender, type) {
+            //cc.log("===> js showBannerAdAtTop action: %d", type);
+            if (type == 2) {
+                var e = upltv.getAccessPrivacyInfoStatus();
+                cc.log("===> js getAccessPrivacyInfoStatus : %d", e);
+                if (e == upltv.GDPRPermissionEnum.UPAccessPrivacyInfoStatusUnkown) {
+                    // 先定位用户是否是欧盟地区
+                    upltv.isEuropeanUnionUser(function(result) {
+                        cc.log("=====> js europeanUnionUserCallBack result: " + result)
+                        // result: "true" 表示欧盟地区用户，否则非欧盟地区用户
+                        if (result == "true") {
+                            // 弹出系统弹窗询问
+                            upltv.notifyAccessPrivacyInfoStatus(function(value) {
+                                cc.log("=====> js notifyAccessPrivacyInfoStatusCallBack callback: %d ", value);
+                                upltv.intSdk(0);
+                            });
+                        } else {
+                            // 非欧盟地区用户，直接初始化SDK
+                            // 假定发行地区是海外，且不需要回调
+                            upltv.intSdk(0);
+                        }
+
+                    });
+                } else {
+                    // 假定发行地区是海外，且不需要回调
+                    upltv.intSdk(0);
+                }
+
+            }
+        },
+        this);
+
+        return true;
+    },
+
+    createButton: function(left, top, text) {
+        var normalImage = "res/feedback.png";
+        var pressedImage = "res/feedback_active.png";
+        var button = new ccui.Button();
+        button.loadTextures(normalImage, pressedImage);
+        button.setText button.attr({
+            x: left,
+            y: top,
+            anchorX: 0.0,
+            anchorY: 1.0
+        });
+        button.setTitleText(text);
+        button.setTitleFontSize(22);
+        this.addChild(button, 1);
+        return button;
+    }
+
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter: function() {
+        this._super();
+        var layer = new HelloWorldLayer();
+        this.addChild(layer);
+    }
+});
---
+++ upltv/UPLTVAndroid.js
@@ -0,0 +1,129 @@
+
+
+var classJavaName = "com/up/ads/cocosjs/JsProxy";
+var showLog = false;
+var upltva = upltva || {
+    setShowLog : function(print) {
+        if (undefined != print && print != null) {
+            showLog = print;
+        }
+    },
+
+    printJsLog : function(msg) {
+        if (showLog && undefined != msg && msg != null) {
+            jsb.reflection.callStaticMethod("android/util/Log", "i", "(Ljava/lang/String;Ljava/lang/String;)I", "cocos2dx-js", msg);
+        }
+    },
+
+    initAndroidSDK : function(zone, vokecall, callname) {
+        //cc.log("===> js initAndroidSDK args zone: %d, call:%s", zone, callname);
+        jsb.reflection.callStaticMethod(classJavaName, "initSDKByZone", "(ILjava/lang/String;)V", zone, callname);
+        jsb.reflection.callStaticMethod(classJavaName, "setInvokeDelegate", "(Ljava/lang/String;)V", vokecall);
+        
+    },
+
+    initAndroidAbtConfigJson : function(gameAccountId, isCompleteTask, isPaid, promotionChannelName, gender, age, tagstring) {
+        //var args = { game_id:gameAccountId, complete:isCompleteTask, paid:isPaid, channel_name:promotionChannelName, gender:gender, age:age, tag:tagstring};
+        //cc.log("===> js initIosAbtConfigJson args string: %s", args.constructor());
+        jsb.reflection.callStaticMethod(classJavaName, "initAbtConfigJsonForJs", "(Ljava/lang/String;ZILjava/lang/String;Ljava/lang/String;ILjava/lang/String;)V",
+                                        gameAccountId, isCompleteTask, isPaid, 
+                                        promotionChannelName, gender, age, tagstring);                               
+    },
+
+    getAndroidAbtConfig : function(cpPlaceId) {
+        return jsb.reflection.callStaticMethod(classJavaName, "getAbtConfig", "(Ljava/lang/String;)Ljava/lang/String;", cpPlaceId);
+    },
+
+    showAndroidRewardDebugUI : function() {
+        jsb.reflection.callStaticMethod(classJavaName, "showRewardDebugActivity", "()V");
+    },
+
+    setAndroidRewardVideoLoadCallback : function() {
+        jsb.reflection.callStaticMethod(classJavaName, "setRewardVideoLoadCallback", "()V");
+    },
+
+    isAndroidRewardReady : function() {
+        return jsb.reflection.callStaticMethod(classJavaName, "isRewardReady", "()Z");
+    },
+
+    showAndroidRewardVideo : function(cpPlaceId) {
+        if (cpPlaceId == null) {
+            cpPlaceId = "reward_video";
+        }
+        jsb.reflection.callStaticMethod(classJavaName, "showRewardVideo", "(Ljava/lang/String;)V", cpPlaceId);
+    },
+
+    setAndroidInterstitialLoadCallback : function(cpPlaceId) {
+        jsb.reflection.callStaticMethod(classJavaName, "setInterstitialCallbackAt", "(Ljava/lang/String;)V", cpPlaceId);
+    },
+
+    isAndroidInterstitialReadyAsyn : function(cpPlaceId, call) {
+        //cc.log("===> isAndroidInterstitialReadyAsyn (%s, %s)", cpPlaceId, call);
+        jsb.reflection.callStaticMethod(classJavaName, "isInterstitialReadyForJs", "(Ljava/lang/String;Ljava/lang/String;)V", cpPlaceId, call);
+    },
+
+    isAndroidInterstitialReady : function(cpPlaceId) {
+        return jsb.reflection.callStaticMethod(classJavaName, "isInterstitialReady", "(Ljava/lang/String;)Z", cpPlaceId);
+    },
+
+    showAndroidInterstitialAd : function(cpPlaceId) {
+        jsb.reflection.callStaticMethod(classJavaName, "showInterstitialForJs", "(Ljava/lang/String;)V", cpPlaceId);
+    },
+
+    showAndroidInterstitialDebugUI : function() {
+        jsb.reflection.callStaticMethod(classJavaName, "showInterstitialDebugActivityForJs", "()V");
+    },
+
+    removeAndroidBannerAdAt : function(cpPlaceId) {
+        jsb.reflection.callStaticMethod(classJavaName, "removeBanner", "(Ljava/lang/String;)V", cpPlaceId);
+    },
+
+    showAndroidBannerAdAtTop : function(cpPlaceId) {
+        jsb.reflection.callStaticMethod(classJavaName, "showTopBanner", "(Ljava/lang/String;)V", cpPlaceId);
+    },
+
+    showAndroidBannerAdAtBottom : function(cpPlaceId) {
+        jsb.reflection.callStaticMethod(classJavaName, "showBottomBanner", "(Ljava/lang/String;)V", cpPlaceId);
+    },
+
+    hideAndroidBannerAdAtTop : function() {
+        jsb.reflection.callStaticMethod(classJavaName, "hideTopBanner", "()V");
+    },
+
+    hideAndroidBannerAdAtBottom : function() {
+        jsb.reflection.callStaticMethod(classJavaName, "hideBottomBanner", "()V");
+    },
+
+    loadAndroidAdsByManual : function() {
+        
+        jsb.reflection.callStaticMethod(classJavaName, "loadAnroidAdsByManual", "()V");
+    },
+
+    exitAndroidApp : function() {
+        jsb.reflection.callStaticMethod(classJavaName, "exitAndroidApp", "()V");
+    },
+
+    setAndroidManifestPackageName : function(pkg) {
+        jsb.reflection.callStaticMethod(classJavaName, "setManifestPackageName", "(Ljava/lang/String;)V", pkg);
+    },
+
+    onAndroidBackPressed : function() {
+        jsb.reflection.callStaticMethod(classJavaName, "onBackPressed", "()V");
+    },
+
+    updateAndroidAccessPrivacyInfoStatus : function(gdprPermissionEnumValue) {
+        jsb.reflection.callStaticMethod(classJavaName, "updateAccessPrivacyInfoStatus", "(I)V", gdprPermissionEnumValue);
+    },
+
+    getAndroidAccessPrivacyInfoStatus : function() {
+       return jsb.reflection.callStaticMethod(classJavaName, "getAccessPrivacyInfoStatus", "()I");
+    },
+
+    notifyAndroidAccessPrivacyInfoStatus : function(callback, callId) {
+        jsb.reflection.callStaticMethod(classJavaName, "notifyAccessPrivacyInfoStatus", "(Ljava/lang/String;I)V", callback, callId);
+    },
+
+    isAndroidEuropeanUnionUser : function(callback, callId) {
+        jsb.reflection.callStaticMethod(classJavaName, "isEuropeanUnionUser", "(Ljava/lang/String;I)V", callback, callId);
+    }
+};
